0f70cf44af2af39d0d53501b8b7204156fd7ba30
Lanc3 <aaron.keating.lanc3@gmail.com>
2023-05-31 16:46:56 +0100

    revert button

--- web/src/components/ExpansionButton/ExpansionButton.js
+++ web/src/components/ExpansionButton/ExpansionButton.js
@@ -1,20 +1,6 @@
-import styles from 'styled-components'
-
 const ExpansionButton = ({ lable, onClickCallback }) => {
-  const Button = styles.button`
-   background: ${(props) => (props.$primary ? '#BF4F74' : 'white')};
-  color: ${(props) => (props.$primary ? 'white' : '#BF4F74')};
-
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid #BF4F74;
-  border-radius: 3px;
-`
-
   return (
     <div>
-      <Button />
       <button
         onClick={onClickCallback}
         className="group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-pink-200 group-hover:from-pink-500 group-hover:to-orange-400 "
